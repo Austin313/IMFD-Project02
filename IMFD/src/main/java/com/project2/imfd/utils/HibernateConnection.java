c42772f61aa105b1c9d61804799690f314f8d943
Austin313 <32691123+Austin313@users.noreply.github.com>
2022-07-23 12:36:11 -0500

    Set up enviornemt variables
    Session factory should compile the connection on new sessions. Should be okay for now.

--- IMFD/src/main/java/com/project2/imfd/utils/HibernateConnection.java
+++ IMFD/src/main/java/com/project2/imfd/utils/HibernateConnection.java
@@ -1,25 +1,43 @@
 package com.project2.imfd.utils;
 
+import java.util.Properties;
+
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
 public class HibernateConnection {
 	 private static StandardServiceRegistry registry;
 	    private static SessionFactory sessionFactory;
+	    private static Configuration config = null;
+	    static String endp = System.getenv("DB_URL");
+	    static String db = System.getenv("DB_Name");
+	    static String user = System.getenv("DB_USER");
+	    static String pass = System.getenv("DB_PASS");
+	    static String url = endp+"/"+db;
+
 	    public static SessionFactory getSessionFactory() {
 	        if (sessionFactory == null) {
 	            try {
-	                // Create registry
-	                registry = new StandardServiceRegistryBuilder().configure().build();
-	                // Create MetadataSources
-	                MetadataSources sources = new MetadataSources(registry);
-	                // Create Metadata
-	                Metadata metadata = sources.getMetadataBuilder().build();
+	            	Properties prop = new Properties();
+	            	prop.put(Environment.DRIVER, "org.postgresql.Driver");
+	            	prop.put(Environment.URL, url);
+	            	prop.put(Environment.USER, user);
+	            	prop.put(Environment.PASS, pass);
+	            	prop.put(Environment.DIALECT, "org.hibernate.dialect.PostgreSQLDialect");
+	            	prop.put(Environment.SHOW_SQL, "true");
+	            	prop.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+	            	prop.put(Environment.HBM2DDL_AUTO, "update");
+	            	config= new Configuration();
+	            	config.setProperties(prop);
+	            	//add configured annotated classes below, using the following format.
+	            	//config.addAnnotatedClass(com.project2.imfd.test.Test.class);
+	           
 	                // Create SessionFactory
-	                sessionFactory = metadata.getSessionFactoryBuilder().build();
+	                sessionFactory = config.buildSessionFactory();
 	            } catch (Exception e) {
 	                e.printStackTrace();
 	                if (registry != null) {
