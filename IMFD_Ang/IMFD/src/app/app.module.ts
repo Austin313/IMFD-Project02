7625a9a4a3c0fb2b5c43adbbdec53f203617d92e
Austin313 <32691123+Austin313@users.noreply.github.com>
2022-07-25 08:58:46 -0500

    uploading angular project
    NOT FULLY FUNCTIONAL

---
+++ IMFD_Ang/IMFD/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ReviewComponent } from './components/review/review.component';
+import { ShopComponent } from './components/shop/shop.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    BlogComponent,
+    ContactComponent,
+    ReviewComponent,
+    ShopComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
