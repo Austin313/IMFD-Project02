636d30bb4c941334fa89833cffefe114e5947a0d
Austin313 <32691123+Austin313@users.noreply.github.com>
2022-07-27 16:45:18 -0500

    added Logging with log4j2
    have not created a place for the logs to go but, will work on that next

--- IMFD/src/main/java/com/project2/imfd/Application.java
+++ IMFD/src/main/java/com/project2/imfd/Application.java
@@ -1,13 +1,18 @@
 package com.project2.imfd;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SuppressWarnings("unused")
 @SpringBootApplication
 public class Application {
-
+	//private static final Logger LOGGER = LogManager.getLogger(Application.class);
 	public static void main(String[] args) {
+		
 		SpringApplication.run(Application.class, args);
+		
 	}
 
 }
--- IMFD/src/main/java/com/project2/imfd/controllers/Home.java
+++ IMFD/src/main/java/com/project2/imfd/controllers/Home.java
@@ -1,17 +1,21 @@
 package com.project2.imfd.controllers;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project2.imfd.Application;
 @SuppressWarnings("unused")
 @Controller
 public class Home {
-	
+	private static Logger LOGGER = LogManager.getLogger(Home.class);
 	@RequestMapping("/home")
 	public String showHome() {
+		LOGGER.info("the home method has been called");
 		return "home";
 	}
-
+	
 }
