f61ef4704df75bb9031bd2daafcc172b2e5d351f
Matson Mathew <matson375@revature.net>
2022-08-03 09:11:14 -0500

    to fetch data for profile
    codes to fetch data for profile, rename passwordd to password

--- IMFD/src/main/java/com/project2/imfd/repo/CustomerRepository.java
+++ IMFD/src/main/java/com/project2/imfd/repo/CustomerRepository.java
@@ -10,6 +10,9 @@ import com.project2.imfd.model.Customer;
 public interface CustomerRepository extends JpaRepository<Customer,Integer> {
 
 	Optional<Customer> findByusername(String uname);
+
+	//public Customer findByUsernameAndPassword(String username, String password);
+	
 	
 
 }
