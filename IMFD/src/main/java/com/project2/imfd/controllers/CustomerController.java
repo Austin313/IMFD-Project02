eab27dee49f4510da852f9b343b778aa66467572
Austin313 <32691123+Austin313@users.noreply.github.com>
2022-08-11 13:53:05 -0500

    update customer

--- IMFD/src/main/java/com/project2/imfd/controllers/CustomerController.java
+++ IMFD/src/main/java/com/project2/imfd/controllers/CustomerController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -69,6 +70,12 @@ public class CustomerController {
 	public ResponseEntity<Customer> getCustomerByUsername(){
 	Customer customer= cs.getCustomerByUsername(username);
 	return new ResponseEntity<>(customer,HttpStatus.OK);
+	}
+	
+	@PutMapping("/update")
+	public ResponseEntity<Customer> updateCustomer(@RequestBody Customer customer ){
+		Customer c = cs.updateCustomer(customer);
+		return new ResponseEntity<>(c,HttpStatus.OK);
 	}
 	
 }
